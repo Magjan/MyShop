8355f36b74c4b36bffc684381087fcb1f43f6e39
Magzhan Sabazbekov <hitch_911@mail.ru>
2020-04-07 09:44:33 +0600

    added product upload

--- MyShop/MyShop.DataAcess.MySQL/SQLRepository.cs
+++ MyShop/MyShop.DataAcess.MySQL/SQLRepository.cs
@@ -3,7 +3,7 @@ using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
-
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,51 +14,51 @@ namespace MyShop.DataAcess.MySQL
     {
         // internal is protected
         internal DataContext context;
-      //  internal DbSet<T> dbSet;
+        internal DbSet<T> dbSet;
 
         public SQLRepository(DataContext context) {
             this.context = context;
-        //    this.dbSet = context.Set<T>();
+            this.dbSet = context.Set<T>();
         }
 
         public IQueryable<T> Collection()
         {
-            //return dbSet;
-            return null;
+            return dbSet;
+           
         }
 
         public void Commit()
         {
-        //    context.SaveChanges();
+           context.SaveChanges();
         }
 
         public void Delete(string Id)
         {
             var t = Find(Id);
 
-         /*   if (context.Entry(t).State==EntityState.Detached) {
+            if (context.Entry(t).State==EntityState.Detached) {
                 dbSet.Attach(t);
             }
 
             dbSet.Remove(t);
-            */
+            
         }
 
         public T Find(string Id)
         {
-            //return dbSet.Find(Id);
-            return null;
+            return dbSet.Find(Id);
+            
         }
 
         public void Insert(T t)
         {
-            //dbSet.Add(t);
+            dbSet.Add(t);
         }
 
         public void Update(T t)
         {
-            //dbSet.Attach(t);
-            //context.Entry(t).State = EntityState.Modified;
+            dbSet.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
         }
     }
 }
--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -44,7 +45,7 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
 
             if (!ModelState.IsValid)
@@ -52,6 +53,15 @@ namespace MyShop.WebUI.Controllers
                 return View(product);
             }
             else {
+
+                // save image file
+                if (file!=null) {
+
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") +product.Image);
+
+                }
+
 
                 context.Insert(product);
                 context.Commit();
@@ -76,7 +86,7 @@ namespace MyShop.WebUI.Controllers
             ProductManagerViewModel viewModel = new ProductManagerViewModel();
 
             // this isn third class which store Product and ProductCategory
-            viewModel.Product = new Product();
+            viewModel.Product = product;
             viewModel.ProductCategories = productCategories.Collection();
 
             return View(viewModel);
@@ -84,7 +94,7 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product, string Id) {
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file) {
 
             Product productToUpdate = context.Find(Id);
 
@@ -103,9 +113,19 @@ namespace MyShop.WebUI.Controllers
                 else
                 {
 
+                    // save image file
+                    if (file != null)
+                    {
+
+                        productToUpdate.Image = product.Id + Path.GetExtension(file.FileName);
+                        file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToUpdate.Image);
+
+                    }
+
+
                     productToUpdate.Category = product.Category;
                     productToUpdate.Description = product.Description;
-                    productToUpdate.Image = product.Image;
+                 
                     productToUpdate.Name = product.Name;
                     productToUpdate.Price = product.Price;
                    
